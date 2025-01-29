3eff8bd927eb3c42a2b26912fb132c559801db51
mayafay <mbaines2023@my.fit.edu>
2025-01-29 17:48:39 -0500

    Update main.c

--- kern/main/main.c
+++ kern/main/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Maya Baines version %s (%s #%d)\n",
+	kprintf("Maya-Baines version %s (%s #%d)\n",
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
